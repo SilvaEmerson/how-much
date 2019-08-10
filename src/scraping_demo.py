d90850a496b4af87f02a1b42b05987d3db983a2a
SilvaEmerson <emersonjose877@gmail.com>
2019-08-09 21:44:03 -0300

    Add show shops method and tests

--- src/scraping_demo.py
+++ src/scraping_demo.py
@@ -7,17 +7,18 @@ import rx
 
 import re
 import os
+from typing import Dict
 
 
-def launch_browser(url, options, config):
+def launch_browser(url: str, options: Options, config: Dict[str, str]) -> webdriver:
     browser = webdriver.Chrome(options=options)
     browser.get(url)
-    delay = 3  # seconds
+    delay = 2  # seconds
 
     try:
         myElem = WebDriverWait(browser, delay, 0.1).until(
             lambda brwoser: brwoser.find_element_by_xpath(
-                config['xpath']["parent"]
+                config["xpath"]["parent"]
             ).is_displayed()
         )
         print("Page is ready!")
@@ -27,7 +28,7 @@ def launch_browser(url, options, config):
     return browser
 
 
-def get_property(element, prop_xpath):
+def get_property(element: webdriver, prop_xpath: str) -> str:
     prop = ""
     try:
         prop = element.find_element_by_xpath(f".{prop_xpath}").text
@@ -37,12 +38,12 @@ def get_property(element, prop_xpath):
     return prop
 
 
-def transform_price(product, regexp):
+def transform_price(product: Dict[str, str], regexp: str) -> Dict[str, str]:
     new_price = re.findall(regexp, product["price"])[0]
     return {**product, "price": new_price}
 
 
-def close_browser(browser):
+def close_browser(browser: webdriver) -> None:
     browser.close()
     browser.quit()
     try:
@@ -50,3 +51,7 @@ def close_browser(browser):
     except:
         pass
 
+def show_shops(config: Dict[str, dict]) -> None:
+    shops = [*config.keys()]
+    temp_str = map(lambda ind, shop: f'[{ind}] {shop}', range(len(shops)), shops)
+    return '\n'.join(temp_str)
--- test/test_scraping_demo.py
+++ test/test_scraping_demo.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import Mock
 
-from src.scraping_demo import get_property
+from src.scraping_demo import show_shops, get_property
 
 
 mocked_parent = Mock()
@@ -9,12 +9,20 @@ mocked_child = Mock()
 
 mocked_parent.find_element_by_xpath.return_value = mocked_child
 
+config_mock = {'magazine': {}, 'americanas': {}, 'amazon': {}}
+
 
 class Main(unittest.TestCase):
     def test_get_property_should_return_demo(self):
-        mocked_child.text = 'demo'
-        self.assertEqual(get_property(mocked_parent, ''), 'demo')
+        mocked_child.text = "demo"
+        self.assertEqual(get_property(mocked_parent, ""), "demo")
 
     def test_get_property_should_return_empty_string(self):
         mocked_parent.find_element_by_xpath.return_value = None
-        self.assertEqual(get_property(mocked_parent, ''), '')
+        self.assertEqual(get_property(mocked_parent, ""), "")
+    
+    def test_show_shops_should_return_a_not_empty_string(self):
+        self.assertGreater(len(show_shops(config_mock)), 0)
+
+    def test_show_shops_should_be_almost_equal_to_expected_string(self):
+        self.assertRegex(show_shops(config_mock), '\[\d+\]\s\w+\n\[\d+\]\s\w+\n\[\d+\]\s\w+')
