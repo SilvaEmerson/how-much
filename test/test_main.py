b7dae628f26b88efbf80b09557f9a41e72bdfa3f
SilvaEmerson <emersonjose877@gmail.com>
2019-08-26 21:08:39 -0300

    Update config.json and test_main.py

--- test/test_main.py
+++ test/test_main.py
@@ -1,18 +1,26 @@
 from rx import operators as ops
 
-import unittest
 from unittest.mock import Mock
+import unittest
+import json
 
 from main import main
 
 
+
+with open('./config.json', 'r') as shops_config:
+    config = json.load(shops_config)
+
+
 shop_picker_input_mock = Mock()
-shop_picker_input_mock.return_value = "1"
 search_term_input_mock = "notebook"
+
 
 
 class Main(unittest.TestCase):
     def test_should_return_no_empty_observable(self):
-        products = main(search_term_input_mock, shop_picker_input_mock)
-        products.subscribe(lambda res: self.assertGreater(len(res), 0))
-        products.connect()
+        for id, shop_name in enumerate(config):
+            shop_picker_input_mock = Mock(return_value=f'{id}')
+            products = main(search_term_input_mock, shop_picker_input_mock)
+            products.subscribe(lambda res: self.assertGreater(len(res), 0, msg=f'{shop_name} return nothing'))
+            products.connect()
